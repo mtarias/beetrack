4cc93b104fec295e092370f91e54a07b2fe3bc4a
Marco Arias <mtarias@sudaoutdoors.com>
2019-06-12 13:38:28 -0400

    Add validations to model

--- app/models/gps_point.rb
+++ app/models/gps_point.rb
@@ -1,3 +1,16 @@
 class GpsPoint < ActiveRecord::Base
     belongs_to :vehicle
+
+    validates :latitude, presence: true
+    validates :longitude, presence: true
+
+    before_create :latitude_and_longitude_are_numbers?
+
+    def latitude_and_longitude_are_numbers?
+        if (Float(latitude) rescue nil) == nil
+            errors.add(:latitude, 'latitude is not a number')
+        elsif (Float(longitude) rescue nil) == nil
+            errors.add(:longitude, 'longitude is not a number')
+        end
+    end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
 class Vehicle < ActiveRecord::Base
-    has_many :gps_points
+    has_many :gps_points, -> { order 'sent_at desc' }
+
+    validates :vehicle_identifier, presence: true
 end
